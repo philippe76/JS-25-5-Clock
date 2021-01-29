3aed977b82055fff7a0aad25d405fca04dc0081e
Philippe Lanougadere <soufiane92230@gmail.com>
2021-01-29 12:05:01 +0100

    Create general style

--- app.js
+++ app.js
@@ -7,59 +7,92 @@ const App = () => {
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            height: '100vh'
+            height: '100vh',
+            backgroundColor: '#db7093',
+            color: 'white',
+            fontFamily: 'Yusei Magic'
+        },
+        title: {
+            fontSize: '2.5rem',
+            letterSpacing: '0.2rem'
+        },
+        para: {
+            marginBottom: 0
         },
         lengthContainer: {
             display: 'flex',
-            border: '1px solid grey'
+            justifyContent: 'space-around',
+            width: '70%',
+            fontSize: '1.3rem'
         },
         breakContainer: {
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            border: '1px solid grey'
         },
         sessionContainer: {
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center',
-            border: '1px solid grey'
+            alignItems: 'center' 
         },
         timeContainer: {
             display: 'flex',
+            width: '60%',
+            justifyContent: 'space-around',
+            alignItems: 'baseline',
         },
         timeLeft: {
-            border: '1px solid grey'
+            border: '3px solid white',
+            padding: '0.5rem 2rem',
+            borderRadius: '8px',
+            marginTop: '2rem',
+            width: '30%',
+            textAlign: 'center'
+        },
+        sessionTitle: {
+            fontSize: '1.8rem',
+            marginBottom: '0.7rem'
+        },
+        counter: {
+            fontSize: '3rem',
+            marginBottom: '1.2rem'
+        },
+        icon: {
+            fontSize: '1.8rem',
+            padding: '0 1rem',
+            cursor: 'pointer',
+            marginBottom: '1rem'
         }
     }
 
     return (
             <div style={style.container}>
-                <h1>25 + 5 CLOCK</h1>
+                <h1 style={style.title}>25 + 5 CLOCK</h1>
                 <section style={style.lengthContainer}>
                     <div id="break-label" style={style.breakContainer}>
-                        <p>Break Container</p>
+                        <p style={style.para}>Break Container</p>
                         <div style={style.timeContainer}>
-                            <i className="fa fa-arrow-circle-o-up" id="break-increment"></i>
+                            <i className="fa fa-arrow-circle-o-up" id="break-increment" style={style.icon}></i>
                             <p id="break-length">5</p> 
-                            <i className="fa fa-arrow-circle-o-down" id="break-decrement"></i>
+                            <i className="fa fa-arrow-circle-o-down" id="break-decrement" style={style.icon}></i>
                         </div>
                     </div>
                     <div id="session-label" style={style.sessionContainer}>
-                        <p>Session Length</p>                        
+                        <p style={style.para}>Session Length</p>                        
                         <div style={style.timeContainer}>
-                            <i className="fa fa-arrow-circle-o-up" id="session-increment"></i>
+                            <i className="fa fa-arrow-circle-o-up" id="session-increment" style={style.icon}></i>
                             <p id="session-length">25</p> 
-                            <i className="fa fa-arrow-circle-o-down" id="session-decrement"></i>
+                            <i className="fa fa-arrow-circle-o-down" id="session-decrement" style={style.icon}></i>
                         </div>
                     </div>
                 </section>
                 <section id="timer-label" style={style.timeLeft}>
-                    <p>Session</p> 
-                    <div id="time-left">25:00</div>
-                    <i className="fa fa-pause" id="start_stop"></i>
-                    <i class="fa fa-refresh" aria-hidden="true"></i>
+                    <p style={style.sessionTitle}>Session</p> 
+                    <div id="time-left" style={style.counter}>25:00</div>
+                    <i className="fa fa-pause" id="start_stop" style={style.icon}></i>
+                    <i className="fa fa-refresh" style={style.icon}></i>
                 </section>
+                <p>by Filip</p>
             </div>
         )
 }
